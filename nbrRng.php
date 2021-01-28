df9b6a707fa09ac8307f59adfd2313de12c85a80
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-01-28 13:42:24 +0100

    add correction

--- nbrRng.php
+++ nbrRng.php
@@ -1,7 +1,7 @@
 <?php
 
     if (isset($_POST["x"], $_POST["y"])){
+        if ($_POST["y"] > getrandmax()) $_POST["y"] = getrandmax();
         echo rand((int)$_POST["x"], (int)$_POST["y"]);
     }
-
 ?>
